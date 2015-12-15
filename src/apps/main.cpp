3171d40701669370f15e2d39e5adcf0ed37ca8e1
Lemaitre Cedric <lemaitrecedric@MacBook-Pro-de-Lemaitre.local>
2015-12-15 22:02:18 +0100

    pour tester fonctionnement de gdb

--- src/apps/main.cpp
+++ src/apps/main.cpp
@@ -1,9 +1,15 @@
-#include "common/dummy.h"
+//#include "common/dummy.h"
+#include <iostream>
 
 int main(int argc, char *argv[]) {
 
     // Create a dummy object
-    DummyClass a;
-
+int32_t val_factoriel = 10;
+int32_t res=1;
+for (int32_t i=1; i<=10; i++)
+{
+    res *=i;
+}
+std::cout << "Valuer de factoriel : " << val_factoriel << " est : " << res;
     return 0;
 }
